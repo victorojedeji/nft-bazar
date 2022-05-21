828aa18bedf3087dca603dfd9672b1673e54a73a
victor_ojedeji <ojedejivictordaniel@gmail.com>
2022-05-21 12:35:31 +0100

    responsiveness

---
+++ index.js
@@ -0,0 +1,13 @@
+// toggle menu
+var nav = document.getElementById("nav-container");
+var open_btn = document.getElementById("open-btn");
+open_btn.onclick= function(){
+nav.style.width = "40%";
+};
+
+var close_btn = document.getElementById("close-btn");
+close_btn.onclick= function(){
+nav.style.width= "0%"
+};
+
+
